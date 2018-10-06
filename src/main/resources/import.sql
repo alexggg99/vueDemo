73dbfd374c8b87346ca2e7118bf73def2bef6389
alexggg99 <agashchuk@corp.local>
2018-10-06 17:12:30 +0300

    server.port + login.html

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
 insert into role(id, name) values (1, 'USER')
 
-insert into user(id, username, password, role_id) values (1, 'user', 123, 1)
-insert into user(id, username, password, role_id) values (2, 'user2', 123, 1)
+insert into user(id, username, password, role_id) values (2, 'user', 123, 1)
+insert into user(id, username, password) values (1, 'fake_user', 123)
